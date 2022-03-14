df141867599771263b53bd16b0758e3ef6d94d2d
DEMBELE96 <saliaasd96@gmail.com>
2022-03-14 14:06:53 -0700

    c-pro

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-int main(void){
-    char ch = 'a';
-	while(ch <= 'z')
+int main(void)
+{
+    
+	for (char ch = 'a'; ch <= 'z'; ch++)
 	{
 		if (ch != 'e' && ch != 'q')
 		{
 			putchar(ch);
 		}
-		ch++;
 	}
 	putchar('\n');
 
